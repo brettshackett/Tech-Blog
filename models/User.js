c883c5f022980b181aa2b0883ce7aef44ab72236
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-06-14 21:50:48 -0400

    fixed spelling mistake causing an error

--- models/User.js
+++ models/User.js
@@ -1,57 +1,72 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require('bcyrpt');
+const bcrypt = require('bcrypt');
 
-class User extends Model{
-    checkPassword(loginPW){
+// create our User model
+class User extends Model {
+    // set up method to run on instance data (per user) to check password 
+    checkPassword(loginPW) {
         return bcrypt.compareSync(loginPW, this.password);
     }
 }
 
-User.init({
+// define table columns and configuration
+User.init(
+  {
     // define an id column
     id: {
-        // use the special Sequelize DataTypes object provide what type of data it is
+        // use the special Sequelize DataTypes object provide what type of data it is 
         type: DataTypes.INTEGER,
-        // this is the equivalent of SQL's `NOT NULL` option
+        // equivalent of SQL "NOT NULL"
         allowNull: false,
         // instruct that this is the Primary Key
         primaryKey: true,
         // turn on auto increment
         autoIncrement: true
-      },
-      // define a username column
-      username: {
+    },
+    // define a username column
+    username: {
         type: DataTypes.STRING,
-        allowNull: false
-      },
-      // define a password column
-      password: {
+        allowNull:false,
+        unique: true
+    },
+    // define a password column 
+    password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          // this means the password must be at least four characters long
-          len: [4]
+            // this means the password must be at least four characters long
+            len: [4]
         }
-      }
-    },
-    {
-        hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
-            async beforeCreate(newUserData) {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
-                
-            },
-            // set up beforeUpdate lifecycle "hook" functionality
-            async beforeUpdate(updatedUserData) {
-                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                return updatedUserData;
-            }
+    }
+  },
+  {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+            
         },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
-})
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
