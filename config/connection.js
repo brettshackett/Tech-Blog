558604ad3c0f1ef130d1578b5b7fa2eb0fcca0d8
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-08-01 20:06:18 -0400

    trying to fix connection issues

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-let sequelize;
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize('tech_blog', 'root', '$hackettBrett2', {
+let sequelize = new Sequelize('tech_blog', 'root', '$hackettBrett2', {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
     });
-}
 module.exports = sequelize;
