e1eec2c0a64b3a5bc4c5f0502c35eebcc1666b0c
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-08-01 20:20:24 -0400

    errors

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
     cookie: { maxAge: 36000 },
-    resave: false,
+    resave: true,
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
-    })
+    }),
 };
 app.use(session(sess));
 app.use(express.json());
